5a65a5d49e1a33b471b4a104a7be26ae89c10b6a
Falco <falco467@gmail.com>
2024-06-10 14:53:24 +0200

    fixed eslint9 config

--- index.js
+++ index.js
@@ -1,46 +1,68 @@
-module.exports = {
-  extends: [
-    'plugin:astro/recommended',
-    'plugin:svelte/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'love',
-  ],
-  rules: {
-    // Needed for svelte reactivity assignment
-    'no-self-assign': 'off',
-    '@typescript-eslint/no-unused-vars': [
-      'error',
-      { "argsIgnorePattern": "^_" }
-    ],
+import eslint from '@eslint/js'
+import globals from 'globals'
+import tsEslint from 'typescript-eslint'
+import epAstro from 'eslint-plugin-astro'
+import astroParser from 'astro-eslint-parser'
+import epSvelte from 'eslint-plugin-svelte'
+import svelteParser from 'svelte-eslint-parser'
+// import love from 'eslint-config-love'
 
-    // Needed because typescript doesn't handle JSDoc correctly
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-unsafe-argument': 'off',
-    '@typescript-eslint/no-unsafe-assignment': 'off',
-    '@typescript-eslint/no-unsafe-call': 'off',
-    '@typescript-eslint/no-unsafe-member-access': 'off',
-    '@typescript-eslint/no-unsafe-return': 'off',
-    '@typescript-eslint/strict-boolean-expressions': 'off',
-  },
-  parserOptions: {
-    project: true,
-    extraFileExtensions: ['.svelte','.astro'],
-  },
-  overrides: [
-    {
-      files: ['*.svelte'],
-      parser: 'svelte-eslint-parser',
+export default [
+  eslint.configs.recommended,
+  ...tsEslint.configs.strictTypeChecked,
+  ...tsEslint.configs.stylisticTypeChecked,
+  ...epAstro.configs['flat/recommended'],
+  ...epSvelte.configs['flat/recommended'],
+  {
+    languageOptions: {
+      parser: tsEslint.parser,
       parserOptions: {
-        parser: '@typescript-eslint/parser',
+        project: true,
+        programs: false,
+        extraFileExtensions: ['.astro','.svelte']
       },
+      globals: {
+        ...globals.browser
+      }
     },
-    {
-      files: ['*.astro'],
-      parser: 'astro-eslint-parser',
+  },
+  {
+    files: ['**/*.astro'],
+    languageOptions: {
+      parser: astroParser,
       parserOptions: {
-        parser: '@typescript-eslint/parser',
-      },
+        parser: tsEslint.parser,
+      }
     },
-  ],
-  ignorePatterns: ['.eslintrc.js', '.eslintrc.cjs'],
-}
+  },
+  {
+    files: ['**/*.svelte'],
+    languageOptions: {
+      parser: svelteParser,
+      parserOptions: {
+        parser: tsEslint.parser,
+      }
+    },
+    rules: {
+      // TODO: JSDoc Types are not handled correctly in Svelte files
+      '@typescript-eslint/no-unsafe-argument': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unsafe-return': 'off',
+      '@typescript-eslint/strict-boolean-expressions': 'off',
+      '@typescript-eslint/restrict-plus-operands': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+    },
+  },
+  {
+    rules: {
+      'no-self-assign': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { "argsIgnorePattern": "^_" }
+      ],
+      '@typescript-eslint/restrict-template-expressions': 'off',
+    },
+  },
+]
