cf906286887a0aab05f352f905ba33c856eca3fe
Falco Wockenfuss <falco467@gmail.com>
2024-07-09 11:23:58 +0200

    Disabled astro parsing due to bug

--- index.js
+++ index.js
@@ -1,39 +1,41 @@
 import eslint from '@eslint/js'
 import globals from 'globals'
 import tsEslint from 'typescript-eslint'
-import epAstro from 'eslint-plugin-astro'
-import astroParser from 'astro-eslint-parser'
+// TODO Astro parsing currently broken with projectService: https://github.com/ota-meshi/astro-eslint-parser/issues/331
+// import epAstro from 'eslint-plugin-astro'
+// import astroParser from 'astro-eslint-parser'
 import epSvelte from 'eslint-plugin-svelte'
 import svelteParser from 'svelte-eslint-parser'
+// TODO: Wait for love to support eslint 9 and flat config: https://github.com/mightyiam/eslint-config-love/issues/1589
 // import love from 'eslint-config-love'
 
 export default [
   eslint.configs.recommended,
   ...tsEslint.configs.strictTypeChecked,
   ...tsEslint.configs.stylisticTypeChecked,
-  ...epAstro.configs['flat/recommended'],
+  // ...epAstro.configs.recommended,
   ...epSvelte.configs['flat/recommended'],
   {
     languageOptions: {
       parser: tsEslint.parser,
       parserOptions: {
         projectService: true,
-        extraFileExtensions: ['.astro','.svelte']
+        extraFileExtensions: ['.svelte'],
       },
       globals: {
         ...globals.browser
       }
     },
   },
-  {
-    files: ['**/*.astro'],
-    languageOptions: {
-      parser: astroParser,
-      parserOptions: {
-        parser: tsEslint.parser,
-      }
-    },
-  },
+  // {
+  //   files: ['**/*.astro'],
+  //   languageOptions: {
+  //     parser: astroParser,
+  //     parserOptions: {
+  //       parser: tsEslint.parser,
+  //     }
+  //   },
+  // },
   {
     files: ['**/*.svelte'],
     languageOptions: {
@@ -43,7 +45,8 @@ export default [
       }
     },
     rules: {
-      // TODO: JSDoc Types are not handled correctly in Svelte files
+      'no-self-assign': 'off',
+      // TODO: JSDoc Types are not handled correctly in Svelte files: https://github.com/sveltejs/svelte-eslint-parser/issues/533
       '@typescript-eslint/no-unsafe-argument': 'off',
       '@typescript-eslint/no-unnecessary-condition': 'off',
       '@typescript-eslint/no-unsafe-assignment': 'off',
@@ -57,7 +60,6 @@ export default [
   },
   {
     rules: {
-      'no-self-assign': 'off',
       '@typescript-eslint/no-unused-vars': [
         'error',
         { "argsIgnorePattern": "^_" }
